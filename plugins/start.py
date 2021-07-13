116f3ad182c20c21d41022b7bb71002d1a0119e4
Aaron-p-saji <65056958+Aaron-p-saji@users.noreply.github.com>
2021-07-13 19:43:53 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Our Other Bots 😊", url="https://t.me/aaronsbotz")],
         [InlineKeyboardButton(
-            "Need Our Repo Link 💀", url="https://t.me/achuz_z2004")]
+            "Need Our Repo Link 💀", url="https://t.me/joinchat/TRVAiXvKv3YyYjBl")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info \n This Bot Developed By <b>Aaron</b>"
     await message.reply_text(welcomed, reply_markup=joinButton)
